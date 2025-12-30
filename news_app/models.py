daa75ca74fd2389673b5459722fd2981e7c9025f
parashac <parashacharya139@gmail.com>
2025-12-30 16:45:19 +0545

    model creation

---
+++ news_app/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+# Create your models here.
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Category(TimeStampModel):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100, null = True, blank = True)
+    description = models.TextField(null=True, blank=True)
+    def __str__(selfself):
+        return self.name
+
+    class Meta;
+    ordering = ["name"]
+    verbose_name = "category"
+    verbose_name_plural = "categories"
+
+class Tag(TimeStampModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(TimeStampModel):
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("inactive", "Inactive"),
+    ]
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    feature_image= models.ImageField(upload_to="post_image/%Y/%m/%d", blank=False)
+    author = models.ForeignKey("auth.user", on_delete=models.CASCADE)
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES, default="active")
+    views_count = models.PositiveBigIntegerField(default=0)
+    is_breaking_news= models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag= models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
