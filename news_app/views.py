0f8eff8c17fece1a407b048d529d9523574a4021
parashac <parashacharya139@gmail.com>
2026-01-04 19:07:28 +0545

    templates

--- news_app/views.py
+++ news_app/views.py
@@ -13,19 +13,19 @@ class HomeView(TemplateView):
 
         context["breaking_news"] = (
                                        Post.objects.filter(
-            published_at__isnull = False, satus= "active", is_breaking_news=True
-        ).ordered_by("-published_at"))[:3]
+            published_at__isnull = False, status= "active", is_breaking_news=True
+        ).order_by("-published_at"))[:3]
 
         context["feature_post"] = (
             Post.objects.filter(published_at__isnull=False, status = "active")
             .order_by("-published_at" , "-views_count"))
         context["trending_news"] = Post.objects.filter(
             published_at__isnull=False, status ="active"
-        ).order_by("publisehd_at")[:4]
+        ).order_by("published_at")[:4]
 
         one_week_ago = timezone.now() - timedelta(days=7)
         context["weekly_top_posts"] = Post.objects.filter(
             published_at__isnull =False, status ="active",published_at__gte =one_week_ago
-        ).order_by("-oublished_at", "-views_count")[:5]
+        ).order_by("-published_at", "-views_count")[:5]
 
         return context
