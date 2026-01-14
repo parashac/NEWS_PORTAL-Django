ff27ac77199e85ab34a7ee2bec9b7711ab948dc8
parashac <parashacharya139@gmail.com>
2026-01-14 17:06:09 +0545

    registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from accounts.forms import LoginForm
-
+from accounts.views import RegisterView
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(authentication_form=LoginForm), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
