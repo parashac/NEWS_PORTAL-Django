ea13ca00d0f2801ec01ca2cb3b7875a452534eb4
parashac <parashacharya139@gmail.com>
2026-01-23 14:56:35 +0545

    content editing at workplace

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news_app'
+    'news_app',
+    'tinymce',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- news_app/admin.py
+++ news_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from tinymce.widgets import TinyMCE
+
 from news_app.models import Post, Category, Tag, Advertisement, Contact, OurTeam, UserProfile, Comment, NewsLetter
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Advertisement)
@@ -11,3 +13,18 @@ admin.site.register(OurTeam)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
 admin.site.register(NewsLetter)
+
+from django import forms
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget = TinyMCE())
+
+    class Meta:
+        model = Post
+        fields ="__all__"
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
